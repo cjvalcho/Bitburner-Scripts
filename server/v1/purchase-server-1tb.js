9d5cc4981b4718aca267021e568d93c7b7a82702
cjvalcho <36437248+cjvalcho@users.noreply.github.com>
2022-03-31 15:11:10 -0700

    Update from game

---
+++ server/v1/purchase-server-1tb.js
@@ -0,0 +1,20 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+	let ram = 1024;
+	let servers = ns.getPurchasedServers();
+	for (let i = 0; i < ns.getPurchasedServerLimit(); i++) {
+		let name = "pserv-" + i;
+		while (ns.getPurchasedServerCost(ram) > ns.getServerMoneyAvailable("home")) {
+			await ns.sleep(3000);
+		}
+		if (servers.includes(name)) {
+			if (ns.getServerMaxRam(name) < ram) {
+				ns.killall(name);
+				ns.deleteServer(name);
+			} else {
+				continue;
+			}
+		}
+		ns.purchaseServer(name, ram);
+	}
+}
